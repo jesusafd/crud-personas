c9b297432a4a410fa7f799af5cbc5e75c341dd05
Jesus Fuentes Diaz <jesusalejandrofuentesdiaz61@gmail.com>
2022-12-12 16:44:00 -0600

    Modelo persona agregado

---
+++ modelos/persona.go
@@ -0,0 +1,11 @@
+package modelos
+
+// Persona es la clase modelo para la base de datos
+type Persona struct {
+	ID_persona int
+	Nombre     string
+	Apellido   string
+	Edad       int
+	Direccion  string
+	Email      string
+}
